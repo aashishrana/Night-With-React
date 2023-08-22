5e8ab8008a46c6a685dc0491dc00ac872f4c9f81
aashishrana <aashishrana71135@gmail.com>
2023-08-22 12:05:39 -0700

    Both add or remove button works fine on clicking the buttons

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,40 +1,34 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  let [counter  , setCounter] = useState(15)
-  // let counter = 15;
-  const addValue  = () => {
+  let [counter, setCounter] = useState(15);
 
+  const addValue = () => {
     console.log("clicked", Math.random());
     counter = counter + 1;
-    
-    setCounter(counter)
-  }
+
+    setCounter(counter);
+  };
 
   const removeValue = () => {
     // console.log("Removed clicked", Math.random());
-    counter = counter -1;
-    
+    counter = counter - 1;
+
     setCounter(counter);
-  }
+  };
 
   return (
     <>
       <h1>Sita Ram</h1>
       <h3>Chai aur React : {counter}</h3>
       <h3>Counter value : {counter}</h3>
-      <button
-       onClick={addValue}>
-        Add value
-      </button>
+      <button onClick={addValue}>Add value</button>
       <br />
-      <button onClick={removeValue}>
-        Remove Value
-      </button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
