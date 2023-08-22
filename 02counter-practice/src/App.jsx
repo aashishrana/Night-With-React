57bde35c8e9114423e8f7427ca7272d3f18abad3
aashishrana <aashishrana71135@gmail.com>
2023-08-22 12:07:14 -0700

    Both add or remove button works fine on clicking the buttons

--- 02counter-practice/src/App.jsx
+++ 02counter-practice/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  // let count  = 10;
+
+  let [count, setCount] = useState(10);
+  let getIncrease = () => {
+    setCount(count + 1);
+  };
+
+  let getDecrease = () => {
+    setCount(count - 1);
+  }
 
   return (
     <>
-      
-      
-      
-     
+      <h1>Jai Shree Ram</h1>
+      <h3>Chai Aur React : {count}</h3>
+
+      <button onClick={getIncrease}>increase</button>
+      <br />
+      <button onClick={getDecrease}>decrease</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
