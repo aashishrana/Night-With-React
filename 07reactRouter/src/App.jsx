48edceabb3cae934db6946e55c265366287075af
Aashish rana <aashishrana71135@gmail.com>
2023-11-24 15:07:12 +0530

    App main file

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -8,11 +8,13 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-orange-500 p-4 ">
-      Sita Ram!
-    </h1>
+      <h1 className="text-3xl font-bold underline bg-orange-500 p-4 ">    
+             Sita Ram!
+      </h1>
     </>
   )
 }
 
 export default App
+
+// main file
