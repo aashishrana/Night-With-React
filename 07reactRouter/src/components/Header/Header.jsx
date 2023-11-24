2dec3b95a004600af0e723e2b8b37b6f5007a409
Aashish rana <aashishrana71135@gmail.com>
2023-11-24 19:56:18 +0530

    Header  component

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import react from "react"
 import {Link , NavLink} from "react-router-dom"
 
 //Link used in place of anchor  tag but anchor tag refresh the whole page that why we use link no refresh
+// Navlink provide additional
 
 export default function Header() {
     return (
@@ -36,11 +37,23 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
+                                    to="/"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200  ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink
+                                    to="/about"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200  ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    About
                                 </NavLink>
                             </li>
                             
