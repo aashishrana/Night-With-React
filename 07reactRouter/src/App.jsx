e9a1fa898a45df9b48f6bcb3124d453450e6d158
Aashish rana <aashishrana71135@gmail.com>
2023-11-24 19:53:57 +0530

    Some changes in app file

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-orange-500 p-4 ">    
+      {/* <h1 className="text-3xl font-bold underline bg-orange-500 p-4 ">    
              Sita Ram!
-      </h1>
+      </h1> */}
+
+      {/* <Header />
+      <Home />
+      <Footer /> */}
     </>
   )
 }
