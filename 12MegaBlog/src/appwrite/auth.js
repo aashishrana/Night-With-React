290ab7f0010285848569264ddac550ba0bea76f0
Aashish rana <aashishrana71135@gmail.com>
2023-12-01 00:15:18 +0530

    Appwrite backend work handle here

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,43 @@
+import conf from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);    
+    }
+
+    async createAccount({email , password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount) {
+                // call another method
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email , password}) {
+        try {
+            await this.account.createEmailSession(email , password);
+            
+
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
