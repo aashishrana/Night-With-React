99ad7cd6cd5a6782bb1b7b2228e68876076469aa
Aashish rana <aashishrana71135@gmail.com>
2023-12-01 16:53:48 +0530

    Authentication with Appwrite

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -18,6 +18,7 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
             if(userAccount) {
                 // call another method
+                return this.login({email , password});
             } else {
                 return userAccount;
             }
@@ -34,6 +35,26 @@ export class AuthService {
 
         } catch (error) {
             throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
