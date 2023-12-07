25721bc87880f1146a8b295fc6ae3a59853a03e3
Aashish rana <aashishrana71135@gmail.com>
2023-12-07 21:25:26 +0530

    Logout btn

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -6,9 +6,13 @@ import { logout } from "../../store/authSlice"
 
 function LogoutBtn() {
     const dispatch = useDispatch()
-    const logoutHandler = () => {}
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
     return (
-        <div>LogoutBtn</div>
+        <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">Logout</button>
     )
 }
 
