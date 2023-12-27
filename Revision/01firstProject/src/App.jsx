6155b5a13fcabbf95cad4ff17c735ae801838829
Aashish rana <aashishrana71135@gmail.com>
2023-12-27 16:23:30 +0530

    Basic project how to render component in main file

---
+++ Revision/01firstProject/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Jai Shree Ram</h1>
+     <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ Revision/01firstProject/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+    return (
+        <>
+          <h1>Chai Component</h1>
+        </> // fragment
+    )
+}
+
+export default Chai
